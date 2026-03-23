bfefeaa0c7928fd864fe80b44e983348044f3b96
j-goodman <johncurtisgoodman@gmail.com>
2026-03-23 14:22:48 -0400

    Add deck contents information.

---
+++ deck-info.js
@@ -0,0 +1,4 @@
+const SUITS = ["clubs", "diamonds", "hearts", "spades"]
+const RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+const RANK_VALUE = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, J: 11, Q: 12, K: 13, A: 14 }
+const SUIT_SORT_ORDER = { clubs: 0, diamonds: 1, spades: 2, hearts: 3 }
