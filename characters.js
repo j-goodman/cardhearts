091b07ab8b079c888caf5a0d32debf2fe26ad929
j-goodman <johncurtisgoodman@gmail.com>
2026-03-25 16:03:07 -0400

    Add character personality data.

--- characters.js
+++ characters.js
@@ -1,5 +1,38 @@
 const CHARACTERS = [
-	{ name: "Ron", folder: "mitch" },
-	{ name: "Mitch", folder: "mitch" },
-	{ name: "Deena", folder: "mitch" }
+    {
+        name: "Mitch",
+        folder: "mitch",
+        personality: {
+            reactionSpeed: .9,
+            reactionDuration: .3,
+            recklessness: .8,
+        }
+    },
+	{
+        name: "Leonard",
+        folder: "leonard",
+        personality: {
+            reactionSpeed: .7,
+            reactionDuration: 1,
+            recklessness: 0,
+        }
+    },
+	{
+        name: "Deena",
+        folder: "deena",
+        personality: {
+            reactionSpeed: 1,
+            reactionDuration: .5,
+            recklessness: .6,
+        }
+    },
+    // {
+    //     name: "Mitch", // devil
+    //     folder: "mitch",
+    //     personality: {
+    //         reactionSpeed: .6,
+    //         reactionDuration: .75,
+    //         recklessness: 1,
+    //     }
+    // },
 ]
