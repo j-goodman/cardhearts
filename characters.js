92d88882e3fe665645dfcbc660314f1ae8b99517
j-goodman <johncurtisgoodman@gmail.com>
2026-03-25 17:53:56 -0400

    Adjust character order.

--- characters.js
+++ characters.js
@@ -1,20 +1,20 @@
 const CHARACTERS = [
     {
-        name: "Mitch",
-        folder: "mitch",
-        personality: {
-            reactionSpeed: .9,
-            reactionDuration: .3,
-            recklessness: .8,
-        }
-    },
-	{
         name: "Leonard",
         folder: "leonard",
         personality: {
             reactionSpeed: .7,
             reactionDuration: 1,
             recklessness: 0,
+        }
+    },
+    {
+        name: "Mitch",
+        folder: "mitch",
+        personality: {
+            reactionSpeed: .9,
+            reactionDuration: .3,
+            recklessness: .8,
         }
     },
 	{
@@ -26,13 +26,4 @@ const CHARACTERS = [
             recklessness: .6,
         }
     },
-    // {
-    //     name: "Mitch", // devil
-    //     folder: "mitch",
-    //     personality: {
-    //         reactionSpeed: .6,
-    //         reactionDuration: .75,
-    //         recklessness: 1,
-    //     }
-    // },
 ]
