7a9389d6555716538d1e2d396fc0a8cff0e67f30
j-goodman <johncurtisgoodman@gmail.com>
2026-04-02 22:44:01 -0400

    Add Val and Terry.

--- characters.js
+++ characters.js
@@ -1,53 +1,68 @@
-let CHARACTERS = [
-    {
-        name: "Leonard",
-        folder: "leonard",
-        personality: {
-            reactionSpeed: .7,
-            reactionDuration: 1,
-            recklessness: .3,
-        }
-    },
-    {
-        name: "Mitch",
-        folder: "mitch",
-        personality: {
-            reactionSpeed: .9,
-            reactionDuration: .3,
-            recklessness: .8,
-        }
-    },
+const CHARACTERS = [
 	{
-        name: "Deena",
-        folder: "deena",
-        personality: {
-            reactionSpeed: 1,
-            reactionDuration: .5,
-            recklessness: .6,
-        }
-    },
+		name: "Deena",
+		folder: "deena",
+        roundDelay: 0,
+		personality: {
+            favoredSuit: "spades",
+			reactionSpeed: 1,
+			reactionDuration: 0.5,
+			recklessness: 0.5
+		}
+	},
+	{
+		name: "Leonard",
+		folder: "leonard",
+        roundDelay: 0,
+		personality: {
+            favoredSuit: "diamonds",
+			reactionSpeed: 0.7,
+			reactionDuration: 1,
+			recklessness: 0.3
+		}
+	},
+	{
+		name: "Mitch",
+		folder: "mitch",
+        roundDelay: 0,
+		personality: {
+            favoredSuit: "hearts",
+			reactionSpeed: 0.9,
+			reactionDuration: 0.4,
+			recklessness: 0.9
+		}
+	},
+	{
+		name: "Sunny",
+		folder: "sunny",
+        roundDelay: 0,
+		personality: {
+            favoredSuit: "clubs",
+			reactionSpeed: 0.3,
+			reactionDuration: 0.9,
+			recklessness: 0
+		}
+	},
+	{
+		name: "Terry",
+		folder: "terry",
+        roundDelay: 2,
+		personality: {
+            favoredSuit: "diamonds",
+			reactionSpeed: 0.5,
+			reactionDuration: 0.7,
+			recklessness: 0.2
+		}
+	},
+    {
+		name: "Val",
+		folder: "val",
+        roundDelay: 1,
+		personality: {
+            favoredSuit: "spades",
+			reactionSpeed: 0.8,
+			reactionDuration: 0.6,
+			recklessness: 1
+		}
+	},
 ]
-
-if (Math.random() > .33) {
-    CHARACTERS[Math.floor(Math.random() * 2)] = {
-        name: "Sunny",
-        folder: "sunny",
-        personality: {
-            reactionSpeed: .3,
-            reactionDuration: .9,
-            recklessness: 0,
-        }
-    }
-}
-
-if (Math.random() > .9) {
-    CHARACTERS[1] = {
-        name: "Terry",
-        folder: "terry",
-        personality: {
-            reactionSpeed: .5,
-            reactionDuration: .7,
-            recklessness: .2,
-        }
-    }
-}
